344da7b4bf8160f5e03667c98649ecea95ef79b6
unknown <>
2018-07-01 21:13:48 +0900

    remove Unnecessary Files

--- src/components/atoms/Balloon/index.js
+++
@@ -1,6 +0,0 @@
- import React from 'react';
- import styles from './styles.css';
-
-const Balloon = ({children, ...props}) => (<span className={styles.balloon}{...props}>{children}</span>);
-
- export default Balloon;
--- src/components/atoms/Img/index.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Img from './index';
-
-export default stories=> stories
-    .add('default', () => <Img src="/mock/images/img01.jpg" width="128" height="72"/>);
